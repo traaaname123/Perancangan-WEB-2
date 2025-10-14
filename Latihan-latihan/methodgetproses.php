8f6f8160dfb5d2a7c151df77b884538421a66635
traaaname123 <sputraadrian15@gmail.com>
2025-10-14 10:17:42 +0700

    Add files via upload

---
+++ Latihan-latihan/methodgetproses.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Method GET proses</title>
+</head>
+<body>
+Data nama yang diinputkan adalah : <?php echo $_GET["nama"]; ?>
+</body>
+</html>
---
+++ Latihan-latihan/variabel.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+
+    <title>Contoh Skrip PHP</title>
+<body>
+<?php
+$nama = "Adrian Saputra";
+echo "$nama berkata ";
+?>
+</body>
+</html>
