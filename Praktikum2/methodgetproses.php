2f99fdcfb4484bd6e7051ec424c0a8e691c58e0f
Adrian Saputra <sputraadrian15@gmail.com>
2025-10-15 17:12:40 +0700

    Delete Praktikum2 directory

--- Praktikum2/methodgetproses.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-<title>Method GET proses</title>
-</head>
-<body>
-Data nama yang diinputkan adalah : <?php echo $_GET["nama"]; ?>
-</body>
-</html>
--- Praktikum2/methodpostproses.php
+++
@@ -1,8 +0,0 @@
-<html> 
-<head> 
-<title> Method POST proses </title> 
-</head> 
-<body> 
-Data nama yang diinputkan adalah : <?php echo $_POST["nama"]?> 
-</body> 
-</html> 
